7ca0a2c7d136b0d44da96b481190dbb1830b2d6d
Roman B <zltcity@gmail.com>
2021-11-07 09:16:56 +0500

    Remove inline specifier for log method.

--- b2-core/include/b2/logger.hpp
+++ b2-core/include/b2/logger.hpp
@@ -25,7 +25,7 @@ public:
 	Logger(const Logger &) = delete;
 	Logger &operator=(const Logger &) = delete;
 
-	inline void log(LogLevel level, const std::string &message) const;
+	void log(LogLevel level, const std::string &message) const;
 
 	std::shared_ptr<Callback> setWriteCallback(Callback callback);
 
@@ -42,29 +42,6 @@ std::string toString(LogLevel level);
 inline void info1(const std::string &message);
 inline void warning(const std::string &message);
 inline void error(const std::string &message);
-
-void Logger::log(LogLevel level, const std::string &message) const
-{
-	auto it = targets.begin();
-
-	while (it != targets.end())
-	{
-		auto callback = it->lock();
-
-		if (callback == nullptr)
-			it = targets.erase(it);
-		else
-		{
-			using namespace std::chrono;
-
-			const auto now = high_resolution_clock::now();
-			const auto fraction = time_point_cast<microseconds>(now).time_since_epoch().count() % uint32_t(1e+6);
-
-			(*callback)(fmt::format("{:%H:%M:%S}.{:0<6} [{:^7}] {}\n", now, fraction, toString(level), message));
-			++it;
-		}
-	}
-}
 
 void info1(const std::string &message)
 {
--- b2-core/src/logger.cpp
+++ b2-core/src/logger.cpp
@@ -3,6 +3,29 @@
 namespace b2
 {
 
+void Logger::log(LogLevel level, const std::string &message) const
+{
+	auto it = targets.begin();
+
+	while (it != targets.end())
+	{
+		auto callback = it->lock();
+
+		if (callback == nullptr)
+			it = targets.erase(it);
+		else
+		{
+			using namespace std::chrono;
+
+			const auto now = high_resolution_clock::now();
+			const auto fraction = time_point_cast<microseconds>(now).time_since_epoch().count() % uint32_t(1e+6);
+
+			(*callback)(fmt::format("{:%H:%M:%S}.{:0<6} [{:^7}] {}\n", now, fraction, toString(level), message));
+			++it;
+		}
+	}
+}
+
 auto Logger::setWriteCallback(Callback callback) -> std::shared_ptr<Callback>
 {
 	auto anchor = std::make_shared<Callback>(std::move(callback));
